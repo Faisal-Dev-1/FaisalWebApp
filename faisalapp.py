f0d47f5f02588cf9ceee04a2b25d3cfa8d0628c8
Faisal-Dev-1 <faisal.alsukiti.01@gmail.com>
2025-06-17 02:45:51 +0400

    Update faisalapp.py

--- faisalapp.py
+++ faisalapp.py
@@ -1,10 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello Faisal! Flask is working.</h1>"
+    return "<h1>Hello from Azure Flask App!</h1>"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
