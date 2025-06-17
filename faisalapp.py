624498724e5203bcf4d49558d048fd0694a0561b
Faisal-Dev-1 <faisal.alsukiti.01@gmail.com>
2025-06-17 15:01:25 +0400

    Update faisalapp.py

--- faisalapp.py
+++ faisalapp.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello Faisal! Flask on Azure using Gunicorn.</h1>"
+    return "<h1>Hello Faisal! Flask with Gunicorn on Linux is running.</h1>"
