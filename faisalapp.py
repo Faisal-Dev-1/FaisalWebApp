3385495c97b936fa64295836a141896ffccd60ed
Faisal-Dev-1 <faisal.alsukiti.01@gmail.com>
2025-06-17 14:52:26 +0400

    Update faisalapp.py

--- faisalapp.py
+++ faisalapp.py
@@ -1,12 +1,7 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Hello Faisal! Flask is running with auto-reload.</h1>"
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=True, use_reloader=True)
+    return "<h1>Hello Faisal! Flask on Azure using Gunicorn.</h1>"
